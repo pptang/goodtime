6ee1f3f09e7acf1408676dd206b7d05003bf835b
Greg Weng <greg.weng@mercari.com>
2019-10-01 21:07:22 +0900

    heap + test

--- heap.js
+++ heap.js
@@ -1,97 +1,385 @@
-// Heap module uses typed array to simulate what G1GC does.
-// We cannot really get benefit from what G1GC achieves,
-// but we just simulate its flow.
+// Heap will have regions.
+// When a new thing needs to be allocated,
+// Allocator will try to re-use regions it keeps.
+// If there is no enough empty regions, it will ask Heap for more.
+// Heap may trigger a minor GC before it gives a new region out.
+// If minor GC is not enough, a full GC will be triggered.
+// The worst case is memory leaks and no region available anymore.
+// Then the Heap will throw a OOM (program crashed)
 
-// We cannot easily have 64bit address since JavaScript has limited safe integer less than that.
-// And we don't have unsigned integer either...
-// Therefore let's just have address < 2^64.
+const REGION_SIZE = 1024000;  // Uint8 * 1024000 = 1MB
+const NUMBER_REGIONS = 256;
 
-const MIN_NUMBER_REGIONS = 256      // 256MB if region size = 262144
-const MIN_REGION_SIZE_32 = 262144;  // 1MB = how many 32bits
-const MAX_ADDRESS = 4294967296;     // 2^32; we can have 16384 regions if it's all by MINs above.
-const NUMBER_EDENS = 4;
+const REGION_EDEN = 1;
+const REGION_SURVIVOR = 2;
+const REGION_TENURED = 3;
+const REGION_HUMOGOUS = 4;
 
-function Heap(numberRegions, configs) {
+const MONO_INT32 = 1;
+const MONO_ADDRESS = 1;  // the same as INT32.
+const MONO_FLOAT64 = 2;  // the largest unit for single slot (8 bytes)
+const MONO_ARRAY_S8 = 3;
+const MONO_STRING_S8 = 4;
+const MONO_OBJECT_S8 = 5;
+const MONO_NAMED_PROPERTY_S8 = 6;   // (addressToStringMono, addressToMono) * 8
 
-    this.regionSize32 = configs.regionSize32 ? configs.regionSize32 : MIN_REGION_SIZE_32;
-    this.numberEdens = configs.numberEdens ? configs.numberEdens : NUMBER_EDENS;
+function Heap() {
 
-    if (numberRegions < this.numberEdens + 1) {
-        throw new Error(
-            "NUmber of regions (" + numberRegions + ") must larger than num of edens ("+ this.numberEdens +") + 1")
+    // We need this 'root' to prevent an unreferenced typed array gone.
+    // This is because we don't control the real memory.
+    this.__rootedContents = [ new Uint8Array(REGION_SIZE) ];
+    for (let i = 0; i < NUMBER_REGIONS; i ++) {
+        this.__rootedContents.push(new Uint8Array(REGION_SIZE));
     }
-    this.numberRegions = numberRegions;
-
-    this.__rootedContents = [ new Uint32Array(this.regionSize32) ];
-
-    // `regionBook` keeps all beging addresses of regions.
-    // It's preserved area will keep alive as long as the program.
-    this.regionBook = new Region(0, this.regionSize32, this.__rootedContents[0]);
-
-    // Yes yes they're contingous since we don't have real memory,
-    // so there is no reason to find holes in the "memory" at the beginning.
-    for (let ieden = 0; ieden < this.numberEdens; ieden += 1) {
-        this.regionBook.intaddr(this.regionBook.endAt + ieden);
-
-        // We need this 'root' to prevent an unreferenced typed array gone.
-        // This is because we don't control the real memory.
-        this.__rootedContents.push(new Uint32Array(this.regionSize32));
-    } 
-
+    this.__contentCounter = 0;
 }
 
-// Each region has 32bit * size32; MIN is 1MB.
-// The min cell in region could be a pointer (address in 32bit) to the real position.
-// `content` is the real place (Uint32Array by `size32`) which may already store somethings.
-// And `content[0]` is alreays a 32bit counter of the last element in the region content array.
-function Region(beginFrom = 0, size32, content) {
+Heap.prototype.createRegion = function() {
+    const content = this.__rootedContents[this.__contentCounter];
+    const beginFrom = this.__contentCounter * REGION_SIZE;
+    return new Region(REGION_EDEN, beginFrom, REGION_SIZE, content);
+}
 
-    if (!!(size32) || size32 < MIN_REGION_SIZE_32) {
-        throw new Error("Minimum region size is 1MB (262144 * 32 bits) (" + size32+ ")");
-    }
-    if (beginFrom + size32 > MAX_ADDRESS) {
-        throw new Error("Region address overflow. At: " + beginFrom+ ", size:" + size32 + "")
-    }
-
-    this.content = content
-    this.size = size32;
+// Regions are now fixed as 1MB by a const.
+// Each region contains Uint8Array with length 
+// GC only cares about regions, and it keeps their information in a preserved area 
+// (since we implement in JS runtime, they're stick to v8's native heap as object properties).
+function Region(kind, beginFrom, size, content) {
+    this.kind = kind;
     this.beginFrom = beginFrom;
-    this.endAt = beginFrom + size32;
+    this.size = size;
+    this.endAt = beginFrom + size - 1;  // index
+    this.content = content;
+    // Read how many Slot/byte (Uint8) has been used.
+    // at any time content[counter] = the last byte has NOT been occupied.
+    this.counter = 0;
+    this.readCounter(); 
 }
 
-// Each region will use the [0] Uint32 as the index of where the last element is.
+// All these read/write functions' `at` is the address/index inside the region (from 0 to 1MB).
+// Cross-region address need to be translated before being used here.
 
-// If this returns false, GC should try to clean this region up,
-// And then if it is still impossible to use the region,
-// heap should try to use another one.
-//
-// If this returns an index (NOT an address),
-// it is the new index can be put something as Int32.
+// And all these write is for host value,
+// while read is also to host value.
+
+Region.prototype.readUint8 = function(at) {
+    if (at > this.size || at < 0) {
+        throw new Error("Read from address out of range: " + at)
+    }
+    return this.content[at];
+}
+
+Region.prototype.readUint32 = function(at) {
+    if (at + 4 > this.size || at < 0) {
+        throw new Error("Read from address out of range: " + at)
+    }
+
+    const buf = new ArrayBuffer(4);
+    const view = new DataView(buf);
+    for (let i = 0; i < 4; i ++ ) {
+        view.setUint8(i, this.content[i + at])
+    }
+    const uint32 = view.getUint32(0);
+    return uint32;
+}
+
+Region.prototype.readInt32 = function(at) {
+    if (at + 4 > this.size || at < 0) {
+        throw new Error("Read from address out of range: " + at)
+    }
+
+    const buf = new ArrayBuffer(4);
+    const view = new DataView(buf);
+    for (let i = 0; i < 4; i ++ ) {
+        view.setUint8(i, this.content[i + at])
+    }
+    const int32 = view.getInt32(0);
+    return int32;
+}
+
+Region.prototype.readAddress = function(at) {
+    return this.readUint32(at);
+}
+
+Region.prototype.readFloat64 = function(at) {
+    if (at + 8 > this.size || at < 0) {
+        throw new Error("Read from address out of range: " + at)
+    }
+
+    const buf = new ArrayBuffer(8);
+    const view = new DataView(buf);
+    for (let i = 0; i < 8; i ++ ) {
+        view.setUint8(i, this.content[i + at])
+    }
+    const float64 = view.getFloat64(0);
+    return float64;
+}
+
+Region.prototype.writeUint8 = function(at, byte) {
+    if (at + 1 > this.size || at < 0) {
+        throw new Error("Write to address out of range: " + at)
+    }
+    this.content[at] = byte;
+}
+
+Region.prototype.writeUint32 = function(at, uint32) {
+    if (at + 4 > this.size || at < 0) {
+        throw new Error("Write to address out of range: " + at)
+    }
+
+    const buf = new ArrayBuffer(4);
+    const view = new DataView(buf);
+    view.setUint32(0, uint32);
+    const temp = new Uint8Array(buf);
+    for (let i = 0; i < 4; i ++ ) {
+        this.content[at + i] = temp[i];
+    }
+}
+
+Region.prototype.writeInt32 = function(at, int32) {
+    if (at + 4 > this.size || at < 0) {
+        throw new Error("Write to address out of range: " + at)
+    }
+
+    const buf = new ArrayBuffer(4);
+    const view = new DataView(buf);
+    view.setInt32(0, int32);
+    const temp = new Uint8Array(buf);
+    for (let i = 0; i < 4; i ++ ) {
+        this.content[at + i] = temp[i];
+    }
+}
+
+Region.prototype.writeAddress = function(at, address) {
+    return this.writeUint32(at, address);
+}
+
+Region.prototype.writeFloat64 = function(at, float64) {
+    if (at + 8 > this.size || at < 0) {
+        throw new Error("Write to address out of range: " + at)
+    }
+
+    const buf = new ArrayBuffer(8);
+    const view = new DataView(buf);
+    view.setFloat64(0, float64);
+    const temp = new Uint8Array(buf);
+    for (let i = 0; i < 8; i ++ ) {
+        this.content[at + i] = temp[i];
+    }
+}
+
+Region.prototype.newUint8 = function(at, uint8) {
+    this.writeUint8(at, uint8);
+    this.counter += 1;
+}
+
+Region.prototype.newUint32 = function(at, uint32) {
+    this.writeUint32(at, uint32);
+    this.counter += 4;
+}
+
+Region.prototype.newInt32 = function(at, int32) {
+    this.writeInt32(at, int32);
+    this.counter += 4;
+}
+
+Region.prototype.newAddress = function(at, address) {
+    this.writeAddress(at, address);
+    this.counter += 4;
+}
+
+Region.prototype.newFloat64 = function(at, float64) {
+    this.writeFloat64(at, float64);
+    this.counter += 8;
+}
+
+// Read the first 4 slots (32bit) to get the counter;
+Region.prototype.readCounter = function() {
+    let counter = this.readUint32(0);
+    if (0 === counter) { // new region
+        this.counter = 4;
+        console.log("read counter: ", counter, this.counter)
+        this.writeCounter()
+    } else {
+        this.counter = counter;
+    }
+}
+
+Region.prototype.writeCounter = function() {
+    this.writeUint32(0, this.counter);
+}
+
 Region.prototype.capable = function(n = 1) {
-    if (this.content[0]+n > this.size) {
+    if (this.counter+n > this.size) {
         return false;
     }
-    this.content[0] += 1;
-    return this.content[0];
+    return true;
 }
 
-// Allocate an int/address
-Region.prototype.intaddr = function(intaddr) {
-    const at = this.capable();
-    if (at === false) {
-        return false;
+Region.prototype.createMono = function(kind) {
+    console.log("before create mono, counter: ", this.counter);
+    const increase = Mono.prototype.sizeFromKind(kind);
+    if (!this.capable(increase)) {
+        throw new Error("OOM: " + kind);
     }
-    this.content[at] = intaddr;
-    return at;
+    const mono = new Mono(this, kind, this.counter);
+    console.log('mono created: ', kind, '[ ', mono.beginFrom, ' : ', mono.endAt, ' ]');
+    mono.writeHeader();
+    console.log('counter: ', this.counter, ' -> ', this.counter + increase);
+    this.counter += increase;
+    return mono;
 }
 
-// Allocate a character from JS string.
-// Print back from `String.fromCodePoint`
-Region.prototype.char = function(c) {
-    const at = this.capable();
-    if (at === false) {
-        return false;
+// Traverse all monos. One by one call the callback
+Region.prototype.traverse = function(cb) {
+    for(let beginFrom = 4; beginFrom < this.counter;) {    // [0 - 3] is the counter.
+        console.log('try to traverse mono at: ', beginFrom)
+        kind = this.readUint8(beginFrom);
+        if (0 === kind) {
+            // End of monos.
+            break;
+        }
+        mono = new Mono(this, kind, beginFrom);
+        cb(mono);
+        beginFrom = mono.endAt + 1;
     }
-    this.content[at] = c.codePointAt(0)
-    return at;
 }
+
+// Mono is a thing composes of Uint8s with a Header records its
+// length (how many Uint8s it equals to), what's its kind, and size, etc.
+//
+// For kinds with address at the latest 4 bytes, GC may change it if the
+// target is moved.
+//
+// Address could cross regions. Regions have boundary by their
+// `beginFrom` and `endAt` properties, so just have a address over the boundary
+// meaning a cross-region reference.
+//
+// Address must point to Mono's header.
+function Mono(region, kind, beginFrom) {
+    this.region = region;
+    this.kind = kind;               // 1 byte
+    this.beginFrom = beginFrom;     // Region knew it when read one by one.
+    this.valueFrom = beginFrom + 1; // 1 byte for header; skip it
+    this.endAt = beginFrom + this.sizeFromKind(kind) - 1; // index so -1
+}
+
+// Write header information onto region content.
+// REMEMBER TO CALL THIS: for any newly created Mono!
+Mono.prototype.writeHeader = function() {
+    this.region.writeUint8(this.beginFrom, this.kind);
+}
+
+Mono.prototype.sizeFromKind = function(kind) {
+    switch (kind) {
+        case MONO_INT32:
+        case MONO_ADDRESS:
+            return 5;    // 1 + 4 (header: 1 byte + int32)
+        case MONO_FLOAT64:
+            return 9;   // 1 + 8
+        case MONO_ARRAY_S8:
+            return 69;   // 1 + 8 * 8 + 4 (header + 8 slots + address to next)
+        case MONO_STRING_S8:
+            return 69;   // 1 + 8 * 8 + 4 (header + 8 slots + address to next)
+        case MONO_OBJECT_S8:
+            return 73;   // 1 + 8 * 8  + 4 + 4 (header + 8 slots + address to name/address dict + address to next)
+        case MONO_NAMED_PROPERTY_S8:
+            return 73;   // 1 + (4 + 4) * 8 + 4 (header + address pairs + address to next)
+        default:
+            throw new Error("Wrong Mono kind: " + kind)
+    }
+}
+
+function Allocator(region) {
+    this.region = region;
+}
+
+// Allocate an object:
+// 1. A new Mono is created from a Region and occupies 8 slots with type MONO_OBJECT8.
+// 2. A WrappedObject is returned
+// 3. If user attach more than 7 properties via WrappedObject#attach,
+//    the underlying Mono will create another Mono::MONO_OBJECT8 and point to it to put new properties,
+//    at the last 4 bytes.
+Allocator.prototype.object = function() {
+    const mono = this.region.createMono(MONO_OBJECT_S8);
+    
+}
+
+
+function WrappedObject(mono) {
+    this.mono = mono;
+}
+
+// Int and Float can be directly written to the slots.
+WrappedObject.prototype.write = function(propertyName, hostValue) {
+    
+}
+
+// Otherwise, it is a thing already on the heap.
+WrappedObject.prototype.attach = function(propertyName, mono) {
+
+}
+
+WrappedObject.prototype.detach = function(propertyName) {
+
+}
+
+function WrappedInt32(mono) {
+    this.mono = mono;
+}
+
+WrappedInt32.prototype.read = function() {
+    return this.mono.region.readInt32(this.mono.valueFrom);
+}
+
+WrappedInt32.prototype.write = function(hostFloat64) {
+    return this.mono.region.writeInt32(this.mono.valueFrom, hostFloat64);
+}
+
+function WrappedFloat64(mono) {
+    this.mono = mono;
+}
+
+WrappedFloat64.prototype.read = function() {
+    return this.mono.region.readFloat64(this.mono.valueFrom);
+}
+
+WrappedFloat64.prototype.write = function(hostFloat64) {
+    return this.mono.region.writeFloat64(this.mono.valueFrom, hostFloat64);
+}
+
+function test() {
+    const heap = new Heap();
+    const testRegion = heap.createRegion();
+
+    console.log("head 4 bytes:",
+        testRegion.readUint8(0),
+        testRegion.readUint8(1),
+        testRegion.readUint8(2),
+        testRegion.readUint8(3)
+    );
+
+    for (let i = 0, newMono, hostFloat64; i < 4; i ++) {
+        hostFloat64 = i + 0.91;
+        newMono = testRegion.createMono(MONO_FLOAT64);
+        newWrapped = new WrappedFloat64(newMono);
+        newWrapped.write(hostFloat64)
+        console.log("float64: host value: ", hostFloat64, " read after write: ", newWrapped.read());
+    }
+
+    for (let i = 0, newMono, hostInt32; i < 4; i ++) {
+        hostInt32 = i * -1;
+        newMono = testRegion.createMono(MONO_INT32);
+        newWrapped = new WrappedInt32(newMono);
+        newWrapped.write(hostInt32)
+        console.log("int32: host value: ", hostInt32, " read after write: ", newWrapped.read());
+    }
+
+    console.log("region usage: ", testRegion.counter);
+
+    testRegion.traverse((mono) => {
+        console.log('traverse mono: ', mono.kind, '[ ', mono.beginFrom, ' - ' , mono.endAt, ' ]');
+    })
+}
+
+test();
