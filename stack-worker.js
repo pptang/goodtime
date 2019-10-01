a119af1d93e59bce74aa45335fa2e7085ba0ab6c
pptang <ptang8@bus.illinois.edu>
2019-10-01 12:58:21 +0900

    feat: handle call, member expression and return statement

--- stack-worker.js
+++ stack-worker.js
@@ -25,29 +25,50 @@ const applyOperator = (operator, leftOperand, rightOperand) => {
 const stackMachine = [];
 
 // Question: How to validate the correctness of stack machine?
-function executeProgram(ast) {
+function executeProgram(ast, args) {
+  console.log('current ast:', ast);
+  const variableMap = new Map(
+    ast.params && args
+      ? ast.params.map((identifier, index) => [identifier.name, args[index]])
+      : null,
+  );
   const handleExpression = node => {
-    console.log(`Handle expression ${node.type} at line: ${node.start}`);
     switch (node.type) {
       case 'Literal':
+        console.log(`Handle ${node.type}:${node.value} at line: ${node.start}`);
         // TODO: what if node.value is not a number?
         return node.value;
       case 'BinaryExpression':
+        console.log(
+          `Handle ${node.type}:'${node.operator}' at line: ${node.start}`,
+        );
         return applyOperator(
           node.operator,
           handleExpression(node.left),
           handleExpression(node.right),
         );
       case 'Identifier':
+        console.log(`Handle ${node.type}:${node.name} at line: ${node.start}`);
         return variableMap.get(node.name);
       case 'ArrowFunctionExpression':
+        console.log(`Handle ${node.type} at line: ${node.start}`);
         // Question: What's the better way to store arrow function expression?
+        // Currently, we return root ast node of arrow function and store in the variable map.
         return node;
+      case 'CallExpression':
+        console.log(`Handle ${node.type} at line: ${node.start}`);
+        return executeProgram(handleExpression(node.callee), node.arguments);
+      case 'MemberExpression':
+        console.log(`Handle ${node.type} at line: ${node.start}`);
+        // How to handle embedded method like ArrayExpression?
+        break;
     }
   };
-
-  const variableMap = new Map();
-  ast.body.forEach(node => {
+  let statements = ast.body;
+  if (ast.type === 'ArrowFunctionExpression') {
+    statements = ast.body.body;
+  }
+  statements.forEach(node => {
     switch (node.type) {
       case 'VariableDeclaration':
         node.declarations.forEach(variableDeclarator => {
@@ -63,14 +84,14 @@ function executeProgram(ast) {
         });
         break;
       case 'ExpressionStatement':
+        return handleExpression(node.expression);
+      case 'BlockStatement':
         break;
-      // TODO: Handle other types of statment (function call, if/else, for loop...)
+      case 'ReturnStatement':
+        return handleExpression(node.argument);
+      // TODO: Handle other types of statment (if/else, for loop...)
     }
   });
-
-  const handleStatement = node => {
-    console.log(`Handle statement ${node.type} at line: ${node.start}`);
-  };
 }
 
 module.exports = executeProgram;
